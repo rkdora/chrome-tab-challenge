e4ce727550971895b2d0acda8a570a46663c31c5
ryuto <rryuto5210@gmail.com>
2020-06-24 00:37:36 +0900

    Add link

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,6 +7,12 @@
     <p>いいパソコンの指標って、「Chromeのタブをどれくらい開けるか」ですよね？<br>
       当Webアプリは、Chromeのタブをどれくらい開くことができるかでパソコンのスペック比較する未来を目指し開発を進めてまいります。
     </p>
+
+    <h2>使い方</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/D_2-L0RFXDY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>
+
+    <a href="https://rkdora.hatenablog.com/entry/2020/06/24/003240">[rkdora's Blog] VueでWebアプリを爆速デプロイしていく</a><br>
+    <a href="https://github.com/rkdora/chrome-tab-challenge/blob/master/src/components/Home.vue">ソースコード</a>
   </div>
 </template>
 
