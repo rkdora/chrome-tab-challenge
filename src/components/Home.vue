485aa76590a6f13ddecda415b5e78da98335d5f6
ryuto <rryuto5210@gmail.com>
2020-06-23 23:12:28 +0900

    Add Home

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1>Let's Chrome Tab Challenge!!</h1>
+    <button @click="start()">Start</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  methods: {
+    start() {
+      for (let i = 0; i < 100; i++) {
+        if (window.open('https://www.yahoo.co.jp/')) {
+          console.log('No.' + i + ' success!');
+        } else {
+          console.log('No.' + i + ' blocked.');
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
